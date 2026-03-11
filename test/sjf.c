9365e3e36123f10d1ca61112097e6adfa8ee90bd
Rainier RJ Espinal <rjespinal6404@gmail.com>
2026-03-12 01:06:31 +0800

    test: base version of algorithm implementation

---
+++ test/sjf.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+#define MAX 100
+
+typedef struct {
+    char pid;
+    int arrival;
+    int burst;
+    int completion;
+    int finished;
+} Process;
+
+int main() {
+    Process p[MAX];
+    int n = 0;
+
+    printf("To stop entering processes, type any non-numeric input (e.g., 'stop').\n");
+    printf("To input a text file, use: stcf < sample.txt\n");
+    printf("\nEnter processes (PID Arrival Burst): \n");
+    
+    while (scanf(" %c %d %d", &p[n].pid, &p[n].arrival, &p[n].burst) == 3) {
+        p[n].finished = 0;
+        n++;
+    }
+
+    int completed = 0;
+    int time = 0;
+
+    while (completed < n) {
+        int shortest = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (p[i].arrival <= time && p[i].finished == 0) {
+                if (shortest == -1 || p[i].burst < p[shortest].burst) {
+                    shortest = i;
+                }
+            }
+        }
+
+        if (shortest == -1) {
+            time++;
+            continue;
+        }
+
+        time += p[shortest].burst;
+        p[shortest].completion = time;
+        p[shortest].finished = 1;
+        completed++;
+    }
+
+    printf("\nPID\tArrival\tBurst\tCompletion\tTurnaround\tWaiting\n");
+
+    for (int i = 0; i < n; i++) {
+        int turnaround = p[i].completion - p[i].arrival;
+        int waiting = turnaround - p[i].burst;
+
+        printf("%c\t%d\t%d\t%d\t\t%d\t\t%d\n",
+                p[i].pid,
+                p[i].arrival,
+                p[i].burst,
+                p[i].completion,
+                turnaround,
+                waiting);
+    }
+
+    return 0;
+}
---
+++ test/stcf.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+typedef struct {
+    char pid;
+    int arrival;
+    int burst;
+    int remaining;
+    int completion;
+} Process;
+
+int main() {
+    Process p[MAX];
+    int n = 0;
+
+    printf("To stop entering processes, type any non-numeric input (e.g., 'stop').\n");
+    printf("To input a text file, use: stcf < sample.txt\n");
+    printf("\nEnter processes (PID Arrival Burst): \n");
+
+    while (scanf(" %c %d %d", &p[n].pid, &p[n].arrival, &p[n].burst) == 3) {
+        p[n].remaining = p[n].burst;
+        n++;
+    }
+
+    int completed = 0;
+    int time = 0;
+    int shortest = -1;
+
+    while (completed < n) {
+        shortest = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (p[i].arrival <= time && p[i].remaining > 0) {
+                if (shortest == -1 || p[i].remaining < p[shortest].remaining) {
+                    shortest = i;
+                }
+            }
+        }
+
+        if (shortest == -1) {
+            time++;
+            continue;
+        }
+
+        p[shortest].remaining--;
+        time++;
+
+        if (p[shortest].remaining == 0) {
+            p[shortest].completion = time;
+            completed++;
+        }
+    }
+
+    printf("\nPID\tArrival\tBurst\tCompletion\tTurnaround\tWaiting\n");
+
+    for (int i = 0; i < n; i++) {
+        int turnaround = p[i].completion - p[i].arrival;
+        int waiting = turnaround - p[i].burst;
+
+        printf("%c\t%d\t%d\t%d\t\t%d\t\t%d\n",
+                p[i].pid,
+                p[i].arrival,
+                p[i].burst,
+                p[i].completion,
+                turnaround,
+                waiting);
+    }
+
+    return 0;
+}
